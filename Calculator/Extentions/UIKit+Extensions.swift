80f64a05f5d092ff1e649f079f267ff628b25839
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-03-25 18:39:21 +0100

    Design 1-9 number pad

---
+++ Calculator/Extentions/UIKit+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  UIKit+Extensions.swift
+//  Calculator
+//
+//  Created by Chijioke on 3/25/20.
+//  Copyright © 2020 Chijioke. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        let context = UIGraphicsGetCurrentContext();
+        color.setFill()
+        context!.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        setBackgroundImage(image, for: state);
+    }
+}
+
+extension UIView {
+    func makeRound(radius: CGFloat? = nil) {
+        clipsToBounds = true
+        layer.cornerRadius = radius ?? bounds.height/2
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,15 +6,104 @@
 //  Copyright © 2020 Chijioke. All rights reserved.
 //
 
-import UIKit
+import Stevia
+
+let padding: CGFloat = 20.0
+let rowSize: CGFloat = UIScreen.main.bounds.width/4.0
 
 class ViewController: UIViewController {
 
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view.
-   }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view.
+
+        let numberStack = buildNumberRows()
+        let stack = UIStackView(arrangedSubviews: [numberStack])
+        stack.spacing = padding
+        stack.alignment = .center
+        view.sv(stack)
+        stack.fillContainer()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
 
 
+    func buildNumberRows() -> UIStackView {
+        var rows = [UIStackView]()
+        var currentStack: UIStackView!
+
+        for (index, item) in (1...9).reversed().enumerated() {
+            let button = RoundButton()
+            numberStyle(button)
+            numberConstraints(button)
+            button.setTitle("\(item)", for: .normal)
+
+            if index % 3 == 0 {
+                // reverse items
+                let subviews = currentStack?.arrangedSubviews
+                subviews?.forEach({ $0.removeFromSuperview() })
+                subviews?.reversed().forEach({ currentStack?.addArrangedSubview($0) })
+
+                currentStack = UIStackView(arrangedSubviews: [button])
+                currentStack.spacing = padding
+                currentStack.distribution = .fillEqually
+                currentStack.alignment = .center
+                rows.append(currentStack)
+            } else {
+                currentStack.addArrangedSubview(button)
+            }
+        }
+
+        let zeroButton = RoundButton()
+        numberStyle(zeroButton)
+        zeroButton.setTitle("0", for: .normal)
+        zeroButton.contentHorizontalAlignment = .leading
+        zeroButton.contentEdgeInsets.left = (rowSize-padding/2.0)/2
+        zeroButton.height(rowSize).width((rowSize + padding / 2.0) * 2)
+
+        let decimalButton = RoundButton()
+        numberStyle(decimalButton)
+        numberConstraints(decimalButton)
+        decimalButton.setTitle(".", for: .normal)
+
+        let bottomStack = UIStackView(arrangedSubviews: [zeroButton, decimalButton])
+        bottomStack.spacing = padding
+        bottomStack.alignment = .center
+        bottomStack.distribution = .fill
+
+        rows.append(bottomStack)
+
+        let container = UIStackView(arrangedSubviews: rows)
+        container.distribution = .fillEqually
+        container.spacing = padding
+        container.alignment = .center
+        container.axis = .vertical
+
+        return container
+    }
+
+}
+
+
+let numberStyle: (UIButton) -> Void = { btn in
+    btn.setBackgroundColor(.darkGray, for: .normal)
+    btn.setBackgroundColor(.lightGray, for: .highlighted)
+    btn.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+    btn.setTitleColor(.white, for: .normal)
+}
+
+let numberConstraints: (UIButton) -> Void = { btn in
+    btn.height(rowSize).width(rowSize)
+}
+
+class RoundButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        makeRound()
+    }
 }
 
