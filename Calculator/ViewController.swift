387f64bb68601610436187c5eed164b91b7d9cc2
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-03-25 23:06:57 +0100

    Design keypad with operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,8 @@
 
 import Stevia
 
-let padding: CGFloat = 20.0
-let rowSize: CGFloat = UIScreen.main.bounds.width/4.0
+let padding: CGFloat = 10.0
+let rowSize: CGFloat = (UIScreen.main.bounds.width-padding*4)/4.0
 
 class ViewController: UIViewController {
 
@@ -20,33 +20,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         let numberStack = buildNumberRows()
-        let stack = UIStackView(arrangedSubviews: [numberStack])
+        let uniaryStack = uniaryOperationStack()
+        let rightOperations = buildOperationStack()
+        let leftStack = UIStackView(arrangedSubviews: [uniaryStack, numberStack])
+        leftStack.axis = .vertical
+        leftStack.spacing = padding
+
+        let stack = UIStackView(arrangedSubviews: [leftStack, rightOperations])
+        stack.distribution = .fillProportionally
         stack.spacing = padding
         stack.alignment = .center
         view.sv(stack)
-        stack.fillContainer()
+        stack.fillContainer().centerInContainer()
     }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
-
 
     func buildNumberRows() -> UIStackView {
         var rows = [UIStackView]()
         var currentStack: UIStackView!
 
-        for (index, item) in (1...9).reversed().enumerated() {
+        for (index, item) in (1...9).enumerated() {
             let button = RoundButton()
             numberStyle(button)
             numberConstraints(button)
             button.setTitle("\(item)", for: .normal)
 
             if index % 3 == 0 {
-                // reverse items
-                let subviews = currentStack?.arrangedSubviews
-                subviews?.forEach({ $0.removeFromSuperview() })
-                subviews?.reversed().forEach({ currentStack?.addArrangedSubview($0) })
 
                 currentStack = UIStackView(arrangedSubviews: [button])
                 currentStack.spacing = padding
@@ -58,11 +56,13 @@ class ViewController: UIViewController {
             }
         }
 
+        rows.reverse()
+
         let zeroButton = RoundButton()
         numberStyle(zeroButton)
         zeroButton.setTitle("0", for: .normal)
         zeroButton.contentHorizontalAlignment = .leading
-        zeroButton.contentEdgeInsets.left = (rowSize-padding/2.0)/2
+        zeroButton.contentEdgeInsets.left = (rowSize - padding)/2
         zeroButton.height(rowSize).width((rowSize + padding / 2.0) * 2)
 
         let decimalButton = RoundButton()
@@ -86,13 +86,67 @@ class ViewController: UIViewController {
         return container
     }
 
+    func uniaryOperationStack() -> UIStackView {
+        var rows = [UIButton]()
+
+        for item in ["AC", "±", "%"] {
+            let button = RoundButton()
+            uniaryOperationStyle(button)
+            numberConstraints(button)
+            button.setTitle("\(item)", for: .normal)
+            rows.append(button)
+        }
+
+        let currentStack = UIStackView(arrangedSubviews: rows)
+        currentStack.spacing = padding
+        currentStack.distribution = .fillEqually
+        currentStack.alignment = .center
+
+        return currentStack
+    }
+
+    func buildOperationStack() -> UIStackView {
+        var rows = [UIButton]()
+
+        for item in ["=", "+", "-", "×", "÷"]{
+            let button = RoundButton()
+            operationStyle(button)
+            numberConstraints(button)
+            button.setTitle("\(item)", for: .normal)
+            rows.append(button)
+        }
+
+        let currentStack = UIStackView(arrangedSubviews: rows.reversed())
+        currentStack.axis = .vertical
+        currentStack.spacing = padding
+        currentStack.distribution = .fillEqually
+        currentStack.alignment = .center
+
+        return currentStack
+    }
+}
+
+let fontSize: CGFloat = 45
+let uniaryOperationStyle: (UIButton) -> Void = { btn in
+    btn.setBackgroundColor(UIColor(white: 0.8, alpha: 0.6), for: .normal)
+    btn.setBackgroundColor(.lightGray, for: .highlighted)
+    btn.titleLabel?.font =  UIFont.systemFont(ofSize: fontSize * 0.8, weight: .medium)
+    btn.setTitleColor(.black, for: .normal)
+}
+
+let operationStyle: (UIButton) -> Void = { btn in
+    btn.setBackgroundColor(.orange, for: .normal)
+    btn.setBackgroundColor(UIColor.orange.withAlphaComponent(0.5), for: .highlighted)
+    btn.contentEdgeInsets.bottom = 5
+    btn.titleLabel?.font = UIFont.systemFont(ofSize: fontSize, weight: .regular)
+    btn.setTitleColor(.white, for: .normal)
 }
 
 
 let numberStyle: (UIButton) -> Void = { btn in
-    btn.setBackgroundColor(.darkGray, for: .normal)
+    btn.setBackgroundColor(UIColor(white: 1, alpha: 0.2), for: .normal)
     btn.setBackgroundColor(.lightGray, for: .highlighted)
-    btn.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+    btn.titleLabel?.font = UIFont.systemFont(ofSize: fontSize, weight: .regular)
     btn.setTitleColor(.white, for: .normal)
 }
 
